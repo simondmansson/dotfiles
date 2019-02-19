1f739070cc644eaac98127544ad3cd91497f7a1d
Kalk88 <simondmansson@gmail.com>
2019-02-19 20:52:17 +0100

    Add symlink script

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Set symlink to dotfiles if the files don't already exist in the home directory.
+dotfiles=(".vimrc" ".zshrc")
+echo "Setting up symlinks for ${dotfiles[@]} to home dir."
+for file in ${dotfiles[@]}; do
+	if [ -f $file -a ! -f $HOME/$file ]
+    then
+      ln -s $(pwd)/$file $HOME/$file
+    else 
+      echo "Skipping $file. Already exist in $HOME."
+    fi
+done
+echo "done"

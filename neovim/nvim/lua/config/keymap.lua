ec219f7a57aaacd86275730ca6f9d685d6106cce
kalk <simon.d.mansson@gmail.com>
2025-01-08 21:00:04 +0100

    lsp, formatting, syntax highligthing and floating terminal

--- neovim/nvim/lua/config/keymap.lua
+++ neovim/nvim/lua/config/keymap.lua
@@ -1,5 +1,6 @@
 -- tab width
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.number = true
 
 vim.keymap.set('n', '<Leader>w', ':write<CR>')
--- neovim/nvim/lua/plugins/lsp.lua
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -15,56 +15,38 @@ return {
 		"saghen/blink.cmp",
 	},
 	config = function()
-		vim.api.nvim_create_autocmd('LspAttach', {
-			callback = function(args)
-				local client = vim.lsp.get_client_by_id(args.data.client_id)
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "Fix startup error by disabling semantic tokens for omnisharp",
+			group = vim.api.nvim_create_augroup("OmnisharpHook", {}),
+			callback = function(ev)
+				local client = vim.lsp.get_client_by_id(ev.data.client_id)
 				if not client then return end
-				if client.supports_method('textDocument/formatting') then
-					vim.api.nvim_create_autocmd('BufWritePre', {
-						buffer = args.buf,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-						end
-					})
+				if client.name == "omnisharp" then
+					client.server_capabilities.semanticTokensProvider = nil
 				end
-			end
+			end,
 		})
 
 		local lsp = require('vim.lsp')
 		vim.keymap.set('n', 'gd', lsp.buf.definition)
-		vim.keymap.set('n', 'gh', lsp.buf.hover)
+		vim.keymap.set('n', 'gh', lsp.buf.hover, { silent = true, noremap = true })
+		vim.keymap.set('n', 'gi', lsp.buf.implementation)
+		vim.keymap.set('n', 'gs', lsp.buf.signature_help, { silent = true, noremap = true })
+
 
 		local capabilities = require('blink.cmp').get_lsp_capabilities()
 		local lspconfig = require('lspconfig')
 		lspconfig.lua_ls.setup { capabilities = capabilities }
 		lspconfig.ts_ls.setup { capabilities = capabilities }
 		lspconfig.fsautocomplete.setup { capabilities = capabilities }
-		--require 'lspconfig'.csharp_ls.setup {}
 		lspconfig.omnisharp.setup {
 			capabilities = capabilities,
 			enable_roslyn_analysers = true,
 			enable_import_completion = true,
-			organize_imports_on_format = true,
+			organize_imports_on_format = false,
 			enable_decompilation_support = true,
 			filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' },
 			cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
-			on_attach = function(client, bufnr)
-				-- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1492605642
-				local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
-				for i, v in ipairs(tokenModifiers) do
-					tmp = string.gsub(v, ' ', '_')
-					tokenModifiers[i] = string.gsub(tmp, '-_', '')
-				end
-				local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
-				for i, v in ipairs(tokenTypes) do
-					tmp = string.gsub(v, ' ', '_')
-					tokenTypes[i] = string.gsub(tmp, '-_', '')
-				end
-				on_attach(client, bufnr)
-			end,
-			flags = {
-				debounce_text_changes = 150,
-			}
 		}
 	end
 }
